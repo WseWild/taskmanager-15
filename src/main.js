DimAntushev/taskmanager-14

9.1.6 Подключит провайдер и хранилище

src/main.js
@@ -4,23 +4,31 @@ import BoardPresenter from './presenter/board.js';
 import FilterPresenter from './presenter/filter.js';
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
+import {isOnline} from './utils/common.js';
 import {render, RenderPosition, remove} from './utils/render.js';
+import {toast} from './utils/toast.js';
 import {MenuItem, UpdateType, FilterType} from './const.js';
 import Api from './api/api.js';
+import Store from './api/store.js';
+import Provider from './api/provider.js';
 
 const AUTHORIZATION = 'Basic hS2sd3dfSwcl1sa2j';
 const END_POINT = 'https://14.ecmascript.pages.academy/task-manager';
+const STORE_PREFIX = 'taskmanager-localstorage';
+const STORE_VER = 'v14';
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 const api = new Api(END_POINT, AUTHORIZATION);
-
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 const tasksModel = new TasksModel();
 const filterModel = new FilterModel();
 
 const siteMenuComponent = new SiteMenuView();
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, api);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, apiWithProvider);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
 const handleTaskNewFormClose = () => {
@@ -37,6 +45,11 @@ const handleSiteMenuClick = (menuItem) => {
       boardPresenter.destroy();
       filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
       boardPresenter.init();
+      if (!isOnline()) {
+        toast('You can\'t create new task offline');
+        siteMenuComponent.setMenuItem(MenuItem.TASKS);
+        break;
+      }
       boardPresenter.createTask(handleTaskNewFormClose);
       siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
       break;
@@ -55,7 +68,7 @@ const handleSiteMenuClick = (menuItem) => {
 filterPresenter.init();
 boardPresenter.init();
 
-api.getTasks()
+apiWithProvider.getTasks()
   .then((tasks) => {
     tasksModel.setTasks(UpdateType.INIT, tasks);
     render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
@@ -69,4 +82,13 @@ api.getTasks()
 
 window.addEventListener('load', () => {
   navigator.serviceWorker.register('/sw.js');
+});
+
+window.addEventListener('online', () => {
+  document.title = document.title.replace(' [offline]', '');
+  apiWithProvider.sync();
+});
+
+window.addEventListener('offline', () => {
+  document.title += ' [offline]';
 });

WseWild/taskmanager-15

src/main.js
@@ -4,23 +4,31 @@ import BoardPresenter from './presenter/board.js';
 import FilterPresenter from './presenter/filter.js';
 import TasksModel from './model/tasks.js';
 import FilterModel from './model/filter.js';
+import {isOnline} from './utils/common.js';
 import {render, RenderPosition, remove} from './utils/render.js';
+import {toast} from './utils/toast.js';
 import {MenuItem, UpdateType, FilterType} from './const.js';
 import Api from './api/api.js';
+import Store from './api/store.js';
+import Provider from './api/provider.js';
 
 const AUTHORIZATION = 'Basic hS2sfS44wcl1sa2j';
 const END_POINT = 'https://15.ecmascript.pages.academy/task-manager';
+const STORE_PREFIX = 'taskmanager-localstorage';
+const STORE_VER = 'v15';
+const STORE_NAME = `${STORE_PREFIX}-${STORE_VER}`;
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 const api = new Api(END_POINT, AUTHORIZATION);
-
+const store = new Store(STORE_NAME, window.localStorage);
+const apiWithProvider = new Provider(api, store);
 const tasksModel = new TasksModel();
 const filterModel = new FilterModel();
 
 const siteMenuComponent = new SiteMenuView();
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, api);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel, apiWithProvider);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
 
 const handleTaskNewFormClose = () => {
@@ -37,6 +45,11 @@ const handleSiteMenuClick = (menuItem) => {
       boardPresenter.destroy();
       filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
       boardPresenter.init();
+      if (!isOnline()) {
+        toast('You can\'t create new task offline');
+        siteMenuComponent.setMenuItem(MenuItem.TASKS);
+        break;
+      }
       boardPresenter.createTask(handleTaskNewFormClose);
       siteMenuComponent.getElement().querySelector(`[value=${MenuItem.TASKS}]`).disabled = true;
       break;
@@ -55,7 +68,7 @@ const handleSiteMenuClick = (menuItem) => {
 filterPresenter.init();
 boardPresenter.init();
 
-api.getTasks()
+apiWithProvider.getTasks()
   .then((tasks) => {
     tasksModel.setTasks(UpdateType.INIT, tasks);
     render(siteHeaderElement, siteMenuComponent, RenderPosition.BEFOREEND);
@@ -69,4 +82,13 @@ api.getTasks()
 
 window.addEventListener('load', () => {
   navigator.serviceWorker.register('/sw.js');
+});
+
+window.addEventListener('online', () => {
+  document.title = document.title.replace(' [offline]', '');
+  apiWithProvider.sync();
+});
+
+window.addEventListener('offline', () => {
+  document.title += ' [offline]';
 });